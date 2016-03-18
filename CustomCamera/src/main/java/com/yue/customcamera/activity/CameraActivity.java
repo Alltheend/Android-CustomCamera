ecef0f483a7c2870bb85ee703a8cc9f5126cafb8
yue <1010398694@qq.com>
2016-03-18 22:27:22 +0800

    Initial commit

---
+++ CustomCamera/src/main/java/com/yue/customcamera/activity/CameraActivity.java
@@ -0,0 +1,198 @@
+package com.yue.customcamera.activity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import com.yue.customcamera.R;
+import com.yue.customcamera.utils.CameraUtil;
+
+import java.io.IOException;
+import java.util.List;
+
+public class CameraActivity extends Activity implements SurfaceHolder.Callback, View.OnClickListener {
+    private Camera mCamera;
+    private SurfaceView surfaceView;
+    private SurfaceHolder mHolder;
+    //默认前置或者后置相机 这里暂时设置为前置
+    private int mCameraId = 1;
+    private Context context;
+
+    //屏幕宽高
+    private int screenWidth;
+    private int screenHeight;
+    private Button btn_camera;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        context = this;
+        initView();
+        initData();
+    }
+
+    private void initView() {
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        mHolder = surfaceView.getHolder();
+        mHolder.addCallback(this);
+        btn_camera = (Button) findViewById(R.id.btn_camera);
+        btn_camera.setOnClickListener(this);
+    }
+
+    private void initData() {
+        DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        screenWidth = dm.widthPixels;
+        screenHeight = dm.heightPixels;
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mCamera == null) {
+            mCamera = getCamera(mCameraId);
+            if (mHolder != null) {
+                startPreview(mCamera, mHolder);
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseCamera();
+    }
+
+    /**
+     * 获取Camera实例
+     *
+     * @return
+     */
+    private Camera getCamera(int id) {
+        Camera camera = null;
+        try {
+            camera = Camera.open(id);
+        } catch (Exception e) {
+
+        }
+        return camera;
+    }
+
+    /**
+     * 预览相机
+     */
+    private void startPreview(Camera camera, SurfaceHolder holder) {
+        try {
+            setupCamera(camera);
+            camera.setPreviewDisplay(holder);
+            //亲测的一个方法 基本覆盖所有手机 将预览矫正
+            CameraUtil.getInstance().setCameraDisplayOrientation(this, mCameraId, camera);
+//            camera.setDisplayOrientation(90);
+            camera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void captrue() {
+        mCamera.takePicture(null, null, new Camera.PictureCallback() {
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                //将data 转换为位图 或者你也可以直接保存为文件使用 FileOutputStream
+                //这里我相信大部分都有其他用处把 比如加个水印 后续再讲解
+                Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                CameraUtil.getInstance().setTakePicktrueOrientation(mCameraId, bitmap);
+                //这里打印宽高 就能看到 CameraUtil.getInstance().getPropPictureSize(parameters.getSupportedPictureSizes(), 200);
+                // 这设置的最小宽度影响返回图片的大小 所以这里一般这是1000左右把我觉得
+                Log.d("bitmapWidth==", bitmap.getWidth() + "");
+                Log.d("bitmapHeight==", bitmap.getHeight() + "");
+            }
+        });
+    }
+
+    /**
+     * 设置
+     */
+    private void setupCamera(Camera camera) {
+        Camera.Parameters parameters = camera.getParameters();
+
+        List<String> focusModes = parameters.getSupportedFocusModes();
+        if (focusModes.contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
+            // Autofocus mode is supported 自动对焦
+            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+        }
+
+        //这里第三个参数为最小尺寸 getPropPreviewSize方法会对从最小尺寸开始升序排列 取出所有支持尺寸的最小尺寸
+        Camera.Size previewSize = CameraUtil.getInstance().getPropPreviewSize(parameters.getSupportedPreviewSizes(), 1000);
+        parameters.setPreviewSize(previewSize.width, previewSize.height);
+
+        Camera.Size pictrueSize = CameraUtil.getInstance().getPropPictureSize(parameters.getSupportedPictureSizes(), 1000);
+        parameters.setPictureSize(pictrueSize.width, pictrueSize.height);
+
+        camera.setParameters(parameters);
+
+        Log.d("previewSize.width===", previewSize.width + "");
+        Log.d("previewSize.height===", previewSize.height + "");
+
+        /**
+         * 设置surfaceView的尺寸 因为camera默认是横屏，所以取得支持尺寸也都是横屏的尺寸
+         * 我们在startPreview方法里面把它矫正了过来，但是这里我们设置设置surfaceView的尺寸的时候要注意 previewSize.height<previewSize.width
+         * previewSize.width才是surfaceView的高度
+         * 一般相机都是屏幕的宽度 这里设置为屏幕宽度 高度自适应 你也可以设置自己想要的大小
+         */
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(screenWidth, screenWidth * previewSize.width / previewSize.height);
+        //这里当然可以设置拍照位置 比如居中 我这里就置顶了
+        //params.gravity = Gravity.CENTER;
+        surfaceView.setLayoutParams(params);
+    }
+
+    /**
+     * 释放相机资源
+     */
+    private void releaseCamera() {
+        if (mCamera != null) {
+            mCamera.setPreviewCallback(null);
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        startPreview(mCamera, holder);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        mCamera.stopPreview();
+        startPreview(mCamera, holder);
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        releaseCamera();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_camera:
+                captrue();
+                break;
+        }
+    }
+
+}
